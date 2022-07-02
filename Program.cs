6217e2c6a8df97da3d1ac91e0c8e916e07615848
«Artem <yevdeev@bk.ru>
2022-07-02 23:31:52 +0300

    формируем метод нахождения нужных элементов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] SelectArray(string[] array, int size)
+{
+    int len = array.Length;
+    string[] resultArray = new string[len];
+    int index = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (array[i].Length <= size)
+        {
+            resultArray[index] = array[i];
+            index++;
+        }
+    }
+    return resultArray;
+}
+
