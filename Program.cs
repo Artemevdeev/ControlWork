7397a70b166ce29f64b0905dd1610e53f4dea3f8
«Artem <yevdeev@bk.ru>
2022-07-02 23:51:58 +0300

    готовая версия + печать

--- Program.cs
+++ Program.cs
@@ -14,3 +14,17 @@
     return resultArray;
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+
+string[] array1 = { "hello", "2", ":-)" };
+Console.Write("array1: ");
+PrintArray(array1);
+Console.WriteLine();
+string[] array2 = SelectArray(array1, 3);
+Console.Write("array2: ");
+PrintArray(array2);
